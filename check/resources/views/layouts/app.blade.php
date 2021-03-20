252c720f7267e939ca7102ddd1919949da0b94d4
pashaivanov <ipasha.qq@gmail.com>
2021-03-20 19:50:16 +0300

    navbar + profile

--- check/resources/views/layouts/app.blade.php
+++ check/resources/views/layouts/app.blade.php
@@ -14,16 +14,16 @@
 <body>
 <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">WOLFSKILLS</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+      <a class="nav-item nav-link active" href="home">Главная<span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="contacts">Контакты</a>
+      <a class="nav-item nav-link" href="#">О нас</a>
+      <a class="nav-item nav-link" href="profile">Личный кабинет</a>
     </div>
   </div>
 </nav>
