6470d6949d902781c5effc4f9ba6509d7c9b3946
lordireality <lordireality@gmail.com>
2021-03-20 19:41:16 +0300

    добавлены потерянные файлы бутстрапа

--- check/resources/views/layouts/app.blade.php
+++ check/resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> <!-- для работы бутстрапа -->
-    <script src="{{ asset('/js/app.js') }}"></script> <!-- для работы бутстрапа -->
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> <!-- корневой файл app css -->
+    <link rel="stylesheet" href= "{{ asset('/css/bootstrap.css') }}"> <!-- для работы бутстрапа -->
+    <script src="{{ asset('/js/app.js') }}"></script> <!-- корневой файл app js -->
+    <script src="{{ asset('/js/bootstrap.js') }}"></script> <!-- бутстраповский джиэс -->
     <title>@yield('title')</title>
 </head>
 <body>
