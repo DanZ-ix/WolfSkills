eb8611908239ad37d4d147c1f07a96006254ef7b
DanZ-ix <mizzery@bk.ru>
2021-03-20 17:28:01 +0300

    Добавил 3 странцы и ссылки на них

--- check/resources/views/contacts.blade.php
+++ check/resources/views/contacts.blade.php
@@ -1,1 +1,13 @@
+@extends("layouts.app")
+
+
+@section('title')
+    Хуй соси
+@endsection
+<br>
+<a href="/">Домой</a>
+
+
+@section('content')
 <h1>Contacts</h1>
+@endsection
--- check/resources/views/home.blade.php
+++ check/resources/views/home.blade.php
@@ -1,4 +1,18 @@
+@extends("layouts.app")
+
+
+@section('title')
+    Волки ебатт
+@endsection
+
+@section('content')
 <h1>Главная страница</h1>
 <a href="contacts">Контакты</a>
 <br>
 <a href="about">О нас</a>
+@endsection
+
+@section('aside')
+    @parent
+    <p>Dop txt</p>
+@endsection
