a856b9c279c654fcd966012808733e02c10a9508
lordireality <lordireality@gmail.com>
2021-03-28 15:58:15 +0300

    profile шаблон сделон

--- check/resources/views/profile.blade.php
+++ check/resources/views/profile.blade.php
@@ -8,7 +8,20 @@
 
 @section('content')
 <h1 style="text-align: center;">Личный кабинет</h1>
+<div id="profile" style="background-color:gray;">
+  <div id="Avatarka" style="display:inline-block; width: 25%; vertical-align:top;">
+    <img src="content\images\profiles\tvoieblet.jpg" style="width:100%; height:auto;">
+    <h3 id="rating" style="text-align:center;">♂♂♂♂♂</h3>
+  </div>  
+  <div id="profileBio" style="display:inline-block; width: 74%; vertical-align:top;">
+    <h3>Меня зовут Дядя Вася</h3>
+    <h3>Я живу в навозе</h3>
+    <h3>Я ем какашки</h3>
+  </div>
+</div>
+
+<div id="mainsection">
+    
+</div>
+
 @endsection
-<p><img src="content\images\profiles" width="100" height="200" 
-  alt="йоабльник" align="left" 
-  vspace="5" hspace="5"></p>
