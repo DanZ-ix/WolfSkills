077a9e3c218623e73b7b1ed193c2eb2cd7e5bca9
pashaivanov <ipasha.qq@gmail.com>
2021-03-28 16:15:49 +0300

    add ypoba

--- check/resources/views/profile.blade.php
+++ check/resources/views/profile.blade.php
@@ -7,13 +7,13 @@
 
 
 @section('content')
-<h1 style="text-align: center;">Личный кабинет</h1>
-<div id="profile" style="background-color:gray;">
+<!-- <h1 style="text-align: center;">Личный кабинет</h1> -->
+<div id="profile">
   <div id="Avatarka" style="display:inline-block; width: 25%; vertical-align:top;">
-    <img src="content\images\profiles\tvoieblet.jpg" style="width:100%; height:auto;">
+    <img src="content\images\profiles\tvoieblet.jpg" style=  "border-radius: 100px; width:100%; height:auto;">
     <h3 id="rating" style="text-align:center;">♂♂♂♂♂</h3>
   </div>  
-  <div id="profileBio" style="display:inline-block; width: 74%; vertical-align:top;">
+  <div id="profileBio" style="background-color:gray; display:inline-block; width: 74%; vertical-align:top;">
     <h3>Меня зовут Дядя Вася</h3>
     <h3>Я живу в навозе</h3>
     <h3>Я ем какашки</h3>
