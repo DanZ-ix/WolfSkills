1b30dd0d6c357195bdcf9d6f49fdf4443bce34ba
DanZ-ix <mizzery@bk.ru>
2021-03-31 01:00:38 +0300

    Изменил миграцию, для создания таблицы юзерс, добавил нужные столбцы

--- check/database/migrations/2014_10_12_000000_create_users_table.php
+++ check/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->float('stars');
+            $table->integer('Rating');
+            $table->string('Role');
         });
     }
 
